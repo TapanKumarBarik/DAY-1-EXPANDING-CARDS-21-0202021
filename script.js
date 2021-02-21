4ba37ad9c225288782ef05c352393870d5e887e6
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2021-02-21 09:16:06 +0530

    Day 1 Exapnding Crads

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-const panels = document.querySelectorAll('.panel')
+const panels = document.querySelectorAll(".pannel");
 
-panels.forEach(panel => {
-    panel.addEventListener('click', () => {
-        removeActiveClasses()
-        panel.classList.add('active')
-    })
-})
-
+panels.forEach((pannel) => {
+  pannel.addEventListener("click", () => {
+    removeActiveClasses();
+    // Adding the class
+    pannel.classList.add("active");
+  });
+});
+// Removing the class
 function removeActiveClasses() {
-    panels.forEach(panel => {
-        panel.classList.remove('active')
-    })
+  panels.forEach((pannel) => {
+    pannel.classList.remove("active");
+  });
 }
